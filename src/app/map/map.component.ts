ec7da5137da21a3a144b4fe5417ec9d28a80af85
alan1998 <drakea062@gmail.com>
2017-10-11 22:37:47 +0100

    Bing key added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,7 +15,13 @@ export class MapComponent implements OnInit {
     var map = new ol.Map({
       layers: [
         new ol.layer.Tile({
-          source: new ol.source.OSM()
+          source: new ol.source.BingMaps({
+            key: 'AoZMNXp87zjIQXWvP83jlfYY7vEid3ObC3vg1I01GGPWpQkf6qmoMjLSEhXiSW1o',
+            imagerySet: 'Aerial',
+            // use maxZoom 19 to see stretched tiles instead of the BingMaps
+            // "no photos at this zoom level" tiles
+            maxZoom: 19
+          })
         })
       ],
       target: 'map',
@@ -25,8 +31,8 @@ export class MapComponent implements OnInit {
         })
       }),
       view: new ol.View({
-        center: [0, 0],
-        zoom: 2
+        center: ol.proj.fromLonLat([-1.1,51.5]),
+        zoom: 13
       })
     });
   }
